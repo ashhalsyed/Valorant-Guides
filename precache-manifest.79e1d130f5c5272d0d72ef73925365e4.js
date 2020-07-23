f2200a4d0cff9cc4bf0714e7a20ccd804164e925
ashhalsyed <ashhalsyed@gmail.com>
2020-07-23 13:12:02 -0400

    Updates

---
+++ precache-manifest.79e1d130f5c5272d0d72ef73925365e4.js
@@ -0,0 +1,422 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee78a58e271eb53d1f5987ebded3c0ca",
+    "url": "/Valorant-Guides/index.html"
+  },
+  {
+    "revision": "a2d33bd6ca26a7471c9c",
+    "url": "/Valorant-Guides/static/css/main.cb16d8c7.chunk.css"
+  },
+  {
+    "revision": "41a26de02be39563863d",
+    "url": "/Valorant-Guides/static/js/2.8f8b9409.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Valorant-Guides/static/js/2.8f8b9409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d33bd6ca26a7471c9c",
+    "url": "/Valorant-Guides/static/js/main.ff65731b.chunk.js"
+  },
+  {
+    "revision": "5a5a26b79d9cfe95f718",
+    "url": "/Valorant-Guides/static/js/runtime-main.94b73081.js"
+  },
+  {
+    "revision": "0889d2acca0a60e1c7fc3b5180bd6d8f",
+    "url": "/Valorant-Guides/static/media/aftershock.0889d2ac.svg"
+  },
+  {
+    "revision": "6c72cea508d13f80ebd2e91e6c1276d6",
+    "url": "/Valorant-Guides/static/media/agentsIcon.6c72cea5.svg"
+  },
+  {
+    "revision": "77c6f42901667c30afcb05a88e9d756e",
+    "url": "/Valorant-Guides/static/media/ares.77c6f429.png"
+  },
+  {
+    "revision": "2a663c0a694eacd4354e8e03d459353d",
+    "url": "/Valorant-Guides/static/media/barrier-orb.2a663c0a.svg"
+  },
+  {
+    "revision": "2712f7dd0d1ef577922d85e8f5081b0a",
+    "url": "/Valorant-Guides/static/media/blade-storm.2712f7dd.svg"
+  },
+  {
+    "revision": "6127d38da54b3e83013a272b76a70683",
+    "url": "/Valorant-Guides/static/media/blast-pack.6127d38d.svg"
+  },
+  {
+    "revision": "a9e19c5ad92702961170a3517628240e",
+    "url": "/Valorant-Guides/static/media/blaze.a9e19c5a.svg"
+  },
+  {
+    "revision": "2228c2fb5b7e1ed0963d27628896451f",
+    "url": "/Valorant-Guides/static/media/bodyIcon.2228c2fb.svg"
+  },
+  {
+    "revision": "8e88ba99756a26848267c89ac0c3f9f5",
+    "url": "/Valorant-Guides/static/media/boom-bot.8e88ba99.svg"
+  },
+  {
+    "revision": "26f14efae21d1643fc28b4ae7d9246bc",
+    "url": "/Valorant-Guides/static/media/breach.26f14efa.png"
+  },
+  {
+    "revision": "f054851c260eea40b35dc449ea91e9fc",
+    "url": "/Valorant-Guides/static/media/breachSmall.f054851c.png"
+  },
+  {
+    "revision": "00a404094b1cdd9deca0ee05a175aaac",
+    "url": "/Valorant-Guides/static/media/brimstone.00a40409.png"
+  },
+  {
+    "revision": "3a6ebabad58c2e5e9cabffede295d73c",
+    "url": "/Valorant-Guides/static/media/brimstoneSmall.3a6ebaba.png"
+  },
+  {
+    "revision": "7ef0c9bdd6b2580d2667a201c7fe50c2",
+    "url": "/Valorant-Guides/static/media/bucky.7ef0c9bd.png"
+  },
+  {
+    "revision": "414f5f56c966a8c4048b72720544cd61",
+    "url": "/Valorant-Guides/static/media/bulldog.414f5f56.png"
+  },
+  {
+    "revision": "ae9784881a0de98d78b21d84451bace6",
+    "url": "/Valorant-Guides/static/media/charges1.ae978488.svg"
+  },
+  {
+    "revision": "97c3a2c033acc9872a15e98fc3c7e98a",
+    "url": "/Valorant-Guides/static/media/charges2.97c3a2c0.svg"
+  },
+  {
+    "revision": "14ba191a50d511dbdc14d2ea3d53e015",
+    "url": "/Valorant-Guides/static/media/charges3.14ba191a.svg"
+  },
+  {
+    "revision": "7afb9f2075923cf0bd93ac1a76a2c58c",
+    "url": "/Valorant-Guides/static/media/classic.7afb9f20.png"
+  },
+  {
+    "revision": "aee9e038a22eb9a08554c1fcd96c9494",
+    "url": "/Valorant-Guides/static/media/cloudburst.aee9e038.svg"
+  },
+  {
+    "revision": "6aae252edab7c7881dc397d2dfc7eae7",
+    "url": "/Valorant-Guides/static/media/code.6aae252e.png"
+  },
+  {
+    "revision": "eb1db008435ae4c61bdf93750ef3a9c2",
+    "url": "/Valorant-Guides/static/media/creditsIcon.eb1db008.svg"
+  },
+  {
+    "revision": "d69a2ae6254440356a19639d2d009a8e",
+    "url": "/Valorant-Guides/static/media/curveball.d69a2ae6.svg"
+  },
+  {
+    "revision": "005ddf571e69f45411044358e8935099",
+    "url": "/Valorant-Guides/static/media/cyber-cage.005ddf57.svg"
+  },
+  {
+    "revision": "e66cccbdc7f419adcb55eaafd9f87fb1",
+    "url": "/Valorant-Guides/static/media/cypher.e66cccbd.png"
+  },
+  {
+    "revision": "eb47e807918e025ac9ccb9a4784f1c5f",
+    "url": "/Valorant-Guides/static/media/cypherSmall.eb47e807.png"
+  },
+  {
+    "revision": "06586bf260a28643c9c71f894a5a67aa",
+    "url": "/Valorant-Guides/static/media/dark-cover.06586bf2.svg"
+  },
+  {
+    "revision": "0c57e3b32178b9e6f7e56aa3f5835f5d",
+    "url": "/Valorant-Guides/static/media/fault-line.0c57e3b3.svg"
+  },
+  {
+    "revision": "05c1fcee5f558b7ce9646cca33707b2f",
+    "url": "/Valorant-Guides/static/media/flashpoint.05c1fcee.svg"
+  },
+  {
+    "revision": "8d176366298fef588ee3d24200408607",
+    "url": "/Valorant-Guides/static/media/frenzy.8d176366.png"
+  },
+  {
+    "revision": "46a0abff1d1c76c06eb70b89bf13bf87",
+    "url": "/Valorant-Guides/static/media/from-the-shadows.46a0abff.svg"
+  },
+  {
+    "revision": "a306853ec9e15f1b86d2f0964500e7e2",
+    "url": "/Valorant-Guides/static/media/getGood.a306853e.png"
+  },
+  {
+    "revision": "ab1282272459c353cce03bd7999450b1",
+    "url": "/Valorant-Guides/static/media/ghost.ab128227.png"
+  },
+  {
+    "revision": "bfdec170f95c8cdc73b0a1854f8d88a7",
+    "url": "/Valorant-Guides/static/media/guardian.bfdec170.png"
+  },
+  {
+    "revision": "b42011e1a593d3a60ff1bf60793ac6a3",
+    "url": "/Valorant-Guides/static/media/guidesIcon.b42011e1.svg"
+  },
+  {
+    "revision": "ee9679c46892ed77443be7ab13654cd0",
+    "url": "/Valorant-Guides/static/media/headIcon.ee9679c4.svg"
+  },
+  {
+    "revision": "30d87c9f2b98b57bce8aecd7afb6f7cc",
+    "url": "/Valorant-Guides/static/media/healing-orb.30d87c9f.svg"
+  },
+  {
+    "revision": "9078422362752a034b0818a421bbca38",
+    "url": "/Valorant-Guides/static/media/heavyshield.90784223.png"
+  },
+  {
+    "revision": "bf713f80833d4c0e01edbc66d1a3f7ab",
+    "url": "/Valorant-Guides/static/media/homepageImage1.bf713f80.png"
+  },
+  {
+    "revision": "a9b4fbb9603439a04cfa0a2afc3ebb8f",
+    "url": "/Valorant-Guides/static/media/homepageImage2.a9b4fbb9.png"
+  },
+  {
+    "revision": "137be4ad7fddb719e97e6fadb91e3bbb",
+    "url": "/Valorant-Guides/static/media/homepageImage3.137be4ad.png"
+  },
+  {
+    "revision": "207454383cafec749381c58e8b01e14f",
+    "url": "/Valorant-Guides/static/media/homepageImage4.20745438.png"
+  },
+  {
+    "revision": "95a219690511ff1456e896d11627f8e9",
+    "url": "/Valorant-Guides/static/media/homepageImage5.95a21969.png"
+  },
+  {
+    "revision": "6de613409856ef9e1b409a6e1be088e0",
+    "url": "/Valorant-Guides/static/media/hot-hands.6de61340.svg"
+  },
+  {
+    "revision": "f0150dd8eb03c33371fdb62d7e907497",
+    "url": "/Valorant-Guides/static/media/hunters-fury.f0150dd8.svg"
+  },
+  {
+    "revision": "3369fc559526002ff06b3a44d893d9e4",
+    "url": "/Valorant-Guides/static/media/incendiary.3369fc55.svg"
+  },
+  {
+    "revision": "759492914903c0f6eb19f6e3cfd42d76",
+    "url": "/Valorant-Guides/static/media/jett.75949291.png"
+  },
+  {
+    "revision": "25dab4782b84ef1136d14d69c06af71f",
+    "url": "/Valorant-Guides/static/media/jettSmall.25dab478.png"
+  },
+  {
+    "revision": "2bf82639ce7c734e2c7f5df754cc2bf9",
+    "url": "/Valorant-Guides/static/media/judge.2bf82639.png"
+  },
+  {
+    "revision": "f473cd3eef7b8fd95acbd02f96268eb8",
+    "url": "/Valorant-Guides/static/media/knife.f473cd3e.png"
+  },
+  {
+    "revision": "bc27509e7e0d01549d8299e4cd1751cc",
+    "url": "/Valorant-Guides/static/media/knifeDirection.bc27509e.svg"
+  },
+  {
+    "revision": "589459799c88a9ecc90046dcde277e88",
+    "url": "/Valorant-Guides/static/media/legsIcon.58945979.svg"
+  },
+  {
+    "revision": "fb6ce952480edd98b09f98144849b250",
+    "url": "/Valorant-Guides/static/media/lightshield.fb6ce952.png"
+  },
+  {
+    "revision": "e0a2ea1f1c3e8e45c52bd867a70d9dc6",
+    "url": "/Valorant-Guides/static/media/marshal.e0a2ea1f.png"
+  },
+  {
+    "revision": "ca5af312a31af1d416737d24cf946f0b",
+    "url": "/Valorant-Guides/static/media/neural-theft.ca5af312.svg"
+  },
+  {
+    "revision": "c050c38fdb8b101fc6b81704a8a7fd45",
+    "url": "/Valorant-Guides/static/media/odin.c050c38f.png"
+  },
+  {
+    "revision": "7ceb9f21131c0b70654a8506136f87a9",
+    "url": "/Valorant-Guides/static/media/omen.7ceb9f21.png"
+  },
+  {
+    "revision": "d56988494fc27f74fcc98276ac9cbf1f",
+    "url": "/Valorant-Guides/static/media/omenSmall.d5698849.png"
+  },
+  {
+    "revision": "6a4b1bf0bda3f74b0f2743d92a971afe",
+    "url": "/Valorant-Guides/static/media/operator.6a4b1bf0.png"
+  },
+  {
+    "revision": "7291db2efc8791b0c254edd559177967",
+    "url": "/Valorant-Guides/static/media/orbital-strike.7291db2e.svg"
+  },
+  {
+    "revision": "58bf864b10adba82ac497ad441e7d048",
+    "url": "/Valorant-Guides/static/media/owl-drone.58bf864b.svg"
+  },
+  {
+    "revision": "ebd5bee2977cd6798684ebcd74203fc4",
+    "url": "/Valorant-Guides/static/media/paint-shells.ebd5bee2.svg"
+  },
+  {
+    "revision": "3936c7e145d55b4cfb6fa6e34bded1c8",
+    "url": "/Valorant-Guides/static/media/paranoia.3936c7e1.svg"
+  },
+  {
+    "revision": "9a7f1cca239883454b4deb62782ff5b8",
+    "url": "/Valorant-Guides/static/media/phantom.9a7f1cca.png"
+  },
+  {
+    "revision": "2a5751aaca164818a45dae3392c701bd",
+    "url": "/Valorant-Guides/static/media/pheonix.2a5751aa.png"
+  },
+  {
+    "revision": "8fa56b47774f3009db6e7b92100d40b9",
+    "url": "/Valorant-Guides/static/media/pheonixSmall.8fa56b47.png"
+  },
+  {
+    "revision": "021d6a5124b78d11c428d364b27e1e15",
+    "url": "/Valorant-Guides/static/media/poison-cloud.021d6a51.svg"
+  },
+  {
+    "revision": "c91b14f93b071d724ce61f9c4ba1d4dc",
+    "url": "/Valorant-Guides/static/media/raze.c91b14f9.png"
+  },
+  {
+    "revision": "1bf91cfa3c02dc9ae05a887d61d3abf5",
+    "url": "/Valorant-Guides/static/media/razeSmall.1bf91cfa.png"
+  },
+  {
+    "revision": "0de0a9ea1b9e2da5df9eec9e1814e533",
+    "url": "/Valorant-Guides/static/media/recon-bolt.0de0a9ea.svg"
+  },
+  {
+    "revision": "08aaa7bf6e373f8b6de887b168f76b82",
+    "url": "/Valorant-Guides/static/media/resurrection.08aaa7bf.svg"
+  },
+  {
+    "revision": "cdb78edbf01a8faacaf21dbe15804528",
+    "url": "/Valorant-Guides/static/media/rolling-thunder.cdb78edb.svg"
+  },
+  {
+    "revision": "ca203df5d023cfdce909b56fca0c5047",
+    "url": "/Valorant-Guides/static/media/run-it-back.ca203df5.svg"
+  },
+  {
+    "revision": "39ef0f9e0bf99c5c746377ed676e6c55",
+    "url": "/Valorant-Guides/static/media/sage.39ef0f9e.png"
+  },
+  {
+    "revision": "29bbe33fb5a8846356fde5d940e58d50",
+    "url": "/Valorant-Guides/static/media/sageSmall.29bbe33f.png"
+  },
+  {
+    "revision": "790cbfa2ab4f9c79ce8a683b4b5b3961",
+    "url": "/Valorant-Guides/static/media/sheriff.790cbfa2.png"
+  },
+  {
+    "revision": "95628ab6246499aa4f098608acc251a2",
+    "url": "/Valorant-Guides/static/media/shock-bolt.95628ab6.svg"
+  },
+  {
+    "revision": "762bdbae906dcfbcde94902dbc3c25e1",
+    "url": "/Valorant-Guides/static/media/shorty.762bdbae.png"
+  },
+  {
+    "revision": "aa73821447836a6a904bfe76fc193a9c",
+    "url": "/Valorant-Guides/static/media/showstopper.aa738214.svg"
+  },
+  {
+    "revision": "06ce6cdd8ff0d07a63f29a68b3ad805b",
+    "url": "/Valorant-Guides/static/media/shrouded-step.06ce6cdd.svg"
+  },
+  {
+    "revision": "8e5e6fbd75797b74689b320ad6e77585",
+    "url": "/Valorant-Guides/static/media/sky-smoke.8e5e6fbd.svg"
+  },
+  {
+    "revision": "010c3fe79a40a1d100028b409d1cf16d",
+    "url": "/Valorant-Guides/static/media/slow-orb.010c3fe7.svg"
+  },
+  {
+    "revision": "38b27a12ecfa421762542fee76917f29",
+    "url": "/Valorant-Guides/static/media/snake-bite.38b27a12.svg"
+  },
+  {
+    "revision": "c658c63d6ab5e54ce266c73c3fc23f63",
+    "url": "/Valorant-Guides/static/media/sova.c658c63d.png"
+  },
+  {
+    "revision": "f2c9c6d935d909dc88aba4ebd3c2959c",
+    "url": "/Valorant-Guides/static/media/sovaSmall.f2c9c6d9.png"
+  },
+  {
+    "revision": "5807176ca09237a7220d3b4539fae519",
+    "url": "/Valorant-Guides/static/media/spectre.5807176c.png"
+  },
+  {
+    "revision": "2ede1ba6a69ba00f0a5ea36fe541ce59",
+    "url": "/Valorant-Guides/static/media/spycam.2ede1ba6.svg"
+  },
+  {
+    "revision": "1a780db664fd78d443b6a8123435b9fe",
+    "url": "/Valorant-Guides/static/media/stim-beacon.1a780db6.svg"
+  },
+  {
+    "revision": "38e7bbb5a36cd361b7c7b09a60d8c1d9",
+    "url": "/Valorant-Guides/static/media/stinger.38e7bbb5.png"
+  },
+  {
+    "revision": "532882eb00702158ea2753bd86d4703f",
+    "url": "/Valorant-Guides/static/media/tailwind.532882eb.svg"
+  },
+  {
+    "revision": "ff5f6ca6cb706b592f9cc6833c6abb7a",
+    "url": "/Valorant-Guides/static/media/toxic-screen.ff5f6ca6.svg"
+  },
+  {
+    "revision": "251bafb83ad053eb6b63c15ce1eed7c7",
+    "url": "/Valorant-Guides/static/media/trapwire.251bafb8.svg"
+  },
+  {
+    "revision": "c1147ed5ac79d36d2b3b483db9f89b55",
+    "url": "/Valorant-Guides/static/media/updraft.c1147ed5.svg"
+  },
+  {
+    "revision": "56828e9b3b626bbfdd6a39a71d5a8766",
+    "url": "/Valorant-Guides/static/media/valorantGuidesLogo.56828e9b.svg"
+  },
+  {
+    "revision": "049e197112e112a7b32f3100dc36567c",
+    "url": "/Valorant-Guides/static/media/vandal.049e1971.png"
+  },
+  {
+    "revision": "121aa2b97360e37dd2fc83ec49ab8913",
+    "url": "/Valorant-Guides/static/media/viper.121aa2b9.png"
+  },
+  {
+    "revision": "917b3ab7ff41bc4224c725b36ae5e206",
+    "url": "/Valorant-Guides/static/media/viperSmall.917b3ab7.png"
+  },
+  {
+    "revision": "7fd233a6cca2b12a32429f7dcd0f4a7e",
+    "url": "/Valorant-Guides/static/media/vipers-pit.7fd233a6.svg"
+  },
+  {
+    "revision": "8066b8b1b963902dc70364b0def149ce",
+    "url": "/Valorant-Guides/static/media/weaponsIcon.8066b8b1.svg"
+  }
+]);
